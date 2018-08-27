6c4a424e3d36f2ccb08a6bd387f610a06e43a680
Marwix <allan-ridha@live.se>
2018-08-28 00:58:21 +0200

    first commit

---
+++ XOR/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+
+    ' Created in 2014 by Marwix.
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox2.Text = XOREncryption("Password", TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox3.Text = XORDecryption("Password", TextBox4.Text)
+    End Sub
+
+    Public Function XORDecryption(ByVal CodeKey As String, ByVal DataIn As String) As String
+
+        Dim lonDataPtr As Long
+        Dim strDataOut As String
+        Dim intXOrValue1 As Integer
+        Dim intXOrValue2 As Integer
+
+        For lonDataPtr = 1 To (Len(DataIn) / 2)
+            intXOrValue1 = Val("&H" & (Mid$(DataIn, (2 * lonDataPtr) - 1, 2)))
+            intXOrValue2 = Asc(Mid$(CodeKey, ((lonDataPtr Mod Len(CodeKey)) + 1), 1))
+            strDataOut = strDataOut + Chr(intXOrValue1 Xor intXOrValue2)
+        Next lonDataPtr
+
+        XORDecryption = strDataOut
+    End Function
+
+    Public Function XOREncryption(ByVal CodeKey As String, ByVal DataIn As String) As String
+
+        Dim lonDataPtr As Long
+        Dim strDataOut As String
+        Dim temp As Integer
+        Dim tempstring As String
+        Dim intXOrValue1 As Integer
+        Dim intXOrValue2 As Integer
+
+        For lonDataPtr = 1 To Len(DataIn)
+            intXOrValue1 = Asc(Mid$(DataIn, lonDataPtr, 1))
+            intXOrValue2 = Asc(Mid$(CodeKey, ((lonDataPtr Mod Len(CodeKey)) + 1), 1))
+            temp = (intXOrValue1 Xor intXOrValue2)
+            tempstring = Hex(temp)
+            If Len(tempstring) = 1 Then tempstring = "0" & tempstring
+            strDataOut = strDataOut + tempstring
+        Next lonDataPtr
+        XOREncryption = strDataOut
+
+    End Function
+End Class
---
+++ XOR/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("XOR")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("XOR")> 
+<Assembly: AssemblyCopyright("Copyright ©  2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("577e05de-04ab-45cd-8e12-56a8ad8ba3e5")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
